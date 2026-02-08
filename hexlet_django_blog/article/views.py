5b472a6234667bdc22c35dbf2e5e4c9127bd1715
iratatuii <andreikarpenkin123@gmail.com>
2026-02-08 11:45:31 +0300

    refactor: change def views on class

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-def index(request):
-    return render(request, 'articles/index.html')
+# def index(request):
+#     return render(request, 'articles/index.html')
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "articles/index.html")
--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.HomePageView.as_view()),
     path("articles/", include("hexlet_django_blog.article.urls")),
     path("admin/", admin.site.urls),
     path("about/", views.about),
